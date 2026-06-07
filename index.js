f0f379122c601ca0d1efe2452570d3e01697c2f0
voranzov <voranzov@gmail.com>
2026-06-07 00:02:28 -0400

    db connected

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
+
+//connecting to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
